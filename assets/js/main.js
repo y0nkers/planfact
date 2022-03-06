218bd9efb2302a6f40fe7a10ceb92b7e743aa2e9
Евгений <nashinvo@gmail.com>
2022-03-06 22:36:23 +0300

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,74 @@
 const url = "https://api.planfact.io/api/v1/operationcategories";
 import apiKey from "../../apiKey.json" assert { type: "json" };
-const options = {
+const getOptions = {
+    method: 'GET',
     headers: {
-        "Accept": "application/json",
+        "Content-Type": "application/json",
         "X-ApiKey": apiKey.apiKey
     }
 };
 
+const postOptions = {
+
+};
+
 // let operationCategories = document.getElementById("operationCategories");
 let operationChilds = document.getElementById("operationChilds");
+let accountingArticles = null;
 let activeCategory = null;
+let activeArticle = null;
+let selectArticleCategory = document.getElementById("selectArticleCategory");
 
 // 4432174 Активы 4432214 Доходы 4432209 Капитал 4432197 Обязательства 4432221 Расходы
 // 1. Получить json со всеми категориями операций
 // 2. Для каждой категории проверить, является ли первоначальная категория родительской.
 // 3. Если да, то вывести её и рекурсивно найти все её дочерние категории.
 
-async function getOperationChilds(parentId) {
-    operationChilds.innerHTML = "";
-    makeActive(parentId);
-
-    let response = await fetch(url, options);
+async function getArticlesJSON() {
+    let response = await fetch(url, getOptions);
     if (response.ok) {
         let json = await response.json();
-        let data = json.data;
-        console.log(data);
+        console.log(json.data);
+        return json.data.items;
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+        return null;
+    }
+}
 
-        makeOperationTree(parentId, data.items);
+async function postArticlesJSON() {
+    let title = "test2";
+    let operationCategoryType = "Income";
+    let activityType = "Operating";
+    let parentOperationCategoryId = 4432214;
+    let data = { "title": title, "operationCategoryType": operationCategoryType, "activityType": activityType, "parentOperationCategoryId": parentOperationCategoryId};
+    //console.log(JSON.stringify(data));
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "X-ApiKey": apiKey.apiKey
+        },
+        body: JSON.stringify(data)
+    });
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json);
     } else {
         alert("Ошибка HTTP: " + response.status);
     }
 }
 
-function makeActive(id) {
+async function reloadArticles(parentId) {
+    accountingArticles = await getArticlesJSON();
+    if (accountingArticles != null) {
+        operationChilds.innerHTML = "";
+        makeActiveCategory(parentId);
+        makeOperationTree(parentId, accountingArticles);
+    }
+}
+
+function makeActiveCategory(id) {
     if (activeCategory != null) activeCategory.classList.remove("active");
     activeCategory = document.getElementById("operationCategory" + id);
     activeCategory.classList.add("active");
@@ -43,11 +79,12 @@ function makeOperationTree(parentId, categories, nesting = 0, parentNode = opera
         if (categories[item].parentOperationCategoryId == parentId) { // && categories[item].activityType != 0
             let operationNode = document.createElement("p");
             operationNode.classList.add("operationWrap");
-            let html = "<h5>";
+            let html = '<h5 class="d-inline-block">';
             for (let i = 0; i < nesting; i++) {
                 html += "&#9679;";
             }
             html += categories[item].title + "</h5>";
+            html += '<h5 class="text-end">Редактировать</h5><h5 class="text-end">Удалить </h5>';
             operationNode.innerHTML = html;
             operationNode = parentNode.appendChild(operationNode);
 
@@ -56,7 +93,49 @@ function makeOperationTree(parentId, categories, nesting = 0, parentNode = opera
     }
 }
 
+async function reloadSelect(parentId) {
+    accountingArticles = await getArticlesJSON();
+    if (accountingArticles != null) {
+        while (selectArticleCategory.options.length > 1) selectArticleCategory.remove(1); // Удаляем все пункты кроме первого
+        makeActiveArticle(parentId);
+        makeSelectArticleCategory(parentId, accountingArticles);
+    }
+}
+
+function makeActiveArticle(id) {
+    if (activeArticle != null) activeArticle.classList.remove("active");
+    activeArticle = document.getElementById("createArticle" + id);
+    activeArticle.classList.add("active");
+}
+
+function makeSelectArticleCategory(parentId, categories, nesting = 0) {
+    for (let item in categories) {
+        if (categories[item].parentOperationCategoryId == parentId) {
+            let newOption = document.createElement('option');
+            let html = '';
+            for (let i = 0; i < nesting; i++) {
+                html += "&#9679;";
+            }
+            html = categories[item].title;
+            newOption.innerHTML = html;
+            newOption.id = "option" + categories[item].operationCategoryId;
+            selectArticleCategory.appendChild(newOption);
+            makeSelectArticleCategory(categories[item].operationCategoryId, categories, nesting + 1);
+        }
+    }
+}
+// console.log(document.getElementById('selectArticleCategory').options[document.getElementById('selectArticleCategory').selectedIndex].id);
+async function btnCreateArticle() {
+    document.getElementById('createArticleModal').style.display = 'flex';
+    let id = activeCategory.id.substring("operationCategory".length);
+    reloadSelect(id);
+}
+
+
 window.onload = function () {
-    window.getOperationChilds = getOperationChilds;
-    getOperationChilds(4432214);
+    window.reloadArticles = reloadArticles;
+    window.btnCreateArticle = btnCreateArticle;
+    window.reloadSelect = reloadSelect;
+    reloadArticles(4432214);
+    //postArticlesJSON();
 };
